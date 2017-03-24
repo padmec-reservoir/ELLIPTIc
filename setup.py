303c7b5440484fff1a960954033ee6c91265d159
Guilherme <gpkc@cin.ufpe.br>
2017-03-24 17:22:40 -0300

    Pushing version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="padpy",
-    version='0.0.0',
+    version='0.0.1',
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-cov'],
     packages=find_packages(),
