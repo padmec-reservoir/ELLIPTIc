54a3bfbf221a174b98815087043b6fc3c442c159
Guilherme <gpkc@cin.ufpe.br>
2017-06-20 16:07:48 -0300

    Fixing Preprocess.

--- elliptic/Preprocess/__init__.py
+++ elliptic/Preprocess/__init__.py
@@ -1,9 +1,6 @@
-__all__ = ['run_preprocessor', 'PreprocessorMixins', 'Gmsh', 'OpenFile',
-           'ComputeAdj']
+__all__ = ['run_preprocessor', 'Gmsh', 'OpenFile', 'ComputeAdj']
 
 from run_preprocessor import run_preprocessor
-from . import PreprocessorMixins
-
 from . import Gmsh
 from . import OpenFile
 from . import ComputeAdj
