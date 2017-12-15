5a61ad259c6fdb7c5bb2b40aecebf1aa228be98d
Guilherme <gpkc@cin.ufpe.br>
2017-12-15 18:04:59 -0300

    Test grid passing

--- elliptic/Backend/Python/Mesh/Mesh.py
+++ elliptic/Backend/Python/Mesh/Mesh.py
@@ -1,5 +1,28 @@
 
+class Grid:
+
+    def __init__(self, nx, ny, nz, dx, dy, dz):
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+        self.dx = dx
+        self.dy = dy
+        self.dz = dz
+
+    def get_coord(self, i, j, k):
+        return [i * self.dx, j * self.dy, k * self.dz]
+
+
+class MeshFactory:
+
+    def grid(self, nx, ny, nz, dx, dy, dz):
+        return Grid(nx, ny, nz, dx, dy, dz)
+
+
 class Mesh:
-    
+
     def __init__(self, output_formats, report_format, fields):
         pass
+
+    def mesh_factory(self):
+        return MeshFactory()
--- tests/unit/test_mesh.py
+++ tests/unit/test_mesh.py
@@ -6,12 +6,10 @@ class TestGrid:
 
         my_grid = mf.grid(nx=1, ny=2, nz=3,
                           dx=3, dy=2, dz=1)
-        v0 = my_grid.get_vertex(0, 0, 0)
-        v1 = my_grid.get_vertex(1, 0, 0)
-        v2 = my_grid.get_vertex(0, 1, 0)
+        v0 = my_grid.get_coord(0, 0, 0)
+        v1 = my_grid.get_coord(1, 0, 0)
+        v2 = my_grid.get_coord(0, 1, 0)
 
-        assert v0.coord() == [0.0, 0.0, 0.0]
-        assert v1.coord() == [3.0, 0.0, 0.0]
-        assert v2.coord() == [0.0, 4.0, 0.0]
-
-        elliptic_.set_mesh(my_grid)
+        assert v0 == [0.0, 0.0, 0.0]
+        assert v1 == [3.0, 0.0, 0.0]
+        assert v2 == [0.0, 2.0, 0.0]
