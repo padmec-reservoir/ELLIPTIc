706e67a15d46837d373f1fec315ab9c0a00b67a0
Guilherme Caminha <gpkc@cin.ufpe.br>
2018-02-27 19:09:15 -0300

    Testing Elliptic.

--- tests/unit/test_elliptic.py
+++ tests/unit/test_elliptic.py
@@ -1,27 +1,3 @@
-import pytest
-
-from elliptic.Elliptic import Elliptic
-
-
-@pytest.fixture()
-def mesh_backend(mocker):
-    mesh_backend = mocker.Mock()
-    mesh_backend.get_template_manager.return_value = mocker.sentinel.template_manager
-    mesh_backend.get_backend_builder.return_value = mocker.sentinel.get_backend_builder
-    mesh_backend.get_libraries.return_value = mocker.sentinel.libraries
-    mesh_backend.get_include_dirs.return_value = mocker.sentinel.include_dirs
-    mesh_backend.mesh_builder.return_value = mocker.sentinel.mesh_builder
-
-    return mesh_backend
-
-
-@pytest.fixture()
-def elliptic(mocker, mesh_backend):
-    elliptic_ = Elliptic(mesh_backend, mocker.Mock())
-    elliptic_.set_mesh(mocker.sentinel.mesh)
-
-    return elliptic_
-
 
 class TestElliptic:
 
@@ -35,13 +11,13 @@ class TestElliptic:
             mocker.sentinel.kernel_module, mocker.sentinel.mesh)
 
     def test_get_mesh_template_manager(self, elliptic, mocker):
-        assert elliptic.get_mesh_template_manager() == mocker.sentinel.template_manager
+        assert elliptic.get_mesh_template_manager() is mocker.sentinel.template_manager
 
     def test_get_mesh_backend_libs(self, elliptic, mocker):
-        assert elliptic.get_mesh_backend_libs() == mocker.sentinel.libraries
+        assert elliptic.get_mesh_backend_libs() is mocker.sentinel.libraries
 
     def test_get_mesh_backend_include_dirs(self, elliptic, mocker):
-        assert elliptic.get_mesh_backend_include_dirs() == mocker.sentinel.include_dirs
+        assert elliptic.get_mesh_backend_include_dirs() is mocker.sentinel.include_dirs
 
     def test_mesh_builder(self, elliptic, mocker):
-        assert elliptic.mesh_builder() == mocker.sentinel.mesh_builder
+        assert elliptic.mesh_builder() is mocker.sentinel.mesh_builder
